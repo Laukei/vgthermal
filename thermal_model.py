9399bb20c778f4500662ec38d8fa25cd8178bb0f
Rob Heath <rob@robheath.me.uk>
2016-01-25 17:11:16 +0000

    initial commit; simple working example

---
+++ thermal_model.py
@@ -0,0 +1,169 @@
+import numpy as np
+import pygame
+import matplotlib.pyplot as plt
+
+class ThermalSystem:
+	def __init__(self):
+		self.model = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
+
+		self.rate = 10.0
+		self.base = 5.0
+		self.state = np.zeros_like(self.model,dtype=float)
+		self.sources = []
+		self.exhausts = []
+
+		for r, row in enumerate(self.state):
+			for c, cell in enumerate(row):
+				self.state[r][c] += self.base
+				if self.model[r][c] == 1:
+					self.sources.append([r,c])
+				elif self.model[r][c] == -1:
+					self.exhausts.append([r,c])
+		self.genNeighbours()
+
+	def genNeighbours(self):
+		self.neighbours = []
+		for r in range(self.model.shape[0]):
+			self.neighbours.append([])
+			for c in range(self.model.shape[1]):
+				self.neighbours[-1].append([])
+				if c+1 < self.model.shape[1]:
+					self.neighbours[r][c].append([r,c+1])
+				if c-1 >= 0:
+					self.neighbours[r][c].append([r,c-1])
+				if r+1 < self.model.shape[0]:
+					self.neighbours[r][c].append([r+1,c])
+				if r-1 >= 0:
+					self.neighbours[r][c].append([r-1,c])
+
+	def iterate(self):
+		for source in self.sources:
+			self.state[source[0],source[1]]+= self.rate
+
+		for exhaust in self.exhausts:
+			self.state[exhaust[0],exhaust[1]]/=2.0
+
+		self.flowstate = np.zeros_like(self.model,dtype=float)
+		for r, row in enumerate(self.neighbours):
+			for c, cell in enumerate(row):
+				self.flows = []
+				for n, neighbour in enumerate(cell):
+					if self.state[r][c] > self.state[neighbour[0]][neighbour[1]]:
+						self.flows.append([neighbour,self.state[r][c]-self.state[neighbour[0]][neighbour[1]]])
+				if len(self.flows)>0:
+					for flow in self.flows:
+						self.flowstate[flow[0][0]][flow[0][1]]+=flow[1]/float(len(self.flows)+1)
+						self.flowstate[r][c]-=flow[1]/float(len(self.flows)+1)
+
+		self.state=np.add(self.state,self.flowstate)
+
+	def getState(self):
+		return self.state
+
+	def shape(self):
+		return self.state.shape
+
+
+'''
+for i in range(100):
+	ts.iterate()
+	print ts.getState()
+'''
+
+class Square(pygame.sprite.Sprite):
+	def __init__(self,x,y,w,h,color):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface([w,h])
+		self.image.fill(color)
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+	def setColor(self,color):
+		self.image.fill(color)
+
+def scaled_color(value):
+	'''
+	scale_factor = (255.0/float(maxima-minima))
+	return_value = []
+	for i in range(3):
+		if value<=maxima and value>=minima:
+			return_value.append(int(value*scale_factor))
+		elif value>maxima:
+			return_value.append(255)
+		elif value<minima:
+			return_value.append(0)
+	return tuple(return_value)
+	'''
+	if value > maxima:
+		return WHITE
+	elif value < minima:
+		return BLACK
+	else:
+		return tuple([int(255*x) for x in plt.cm.jet((value-minima)/(maxima-minima))[:-1]])
+
+if __name__=="__main__":
+	pygame.init()
+
+	minima = 0
+	maxima = 50
+	border_size = 0
+
+	ts = ThermalSystem()
+	x_number, y_number = ts.shape()
+
+	WINDOW_AREA = (800,800)
+	BLACK = (0,0,0)
+	WHITE = (255,255,255)
+	screen = pygame.display.set_mode(WINDOW_AREA)
+	pygame.display.set_caption('Thermal Model')
+	clock = pygame.time.Clock()
+
+	all_sprites_list = pygame.sprite.LayeredUpdates()
+	model_square_map = []
+	for r, row in enumerate(ts.getState()):
+		model_square_map.append([])
+		for c, cell in enumerate(row):
+			sq = Square(r*(WINDOW_AREA[0]/x_number)+border_size,c*(WINDOW_AREA[1]/y_number)+border_size,(WINDOW_AREA[0]/x_number)-2*border_size,(WINDOW_AREA[1]/y_number)-2*border_size,scaled_color(cell))
+			model_square_map[-1].append(sq)
+			all_sprites_list.add(sq)
+	done = False
+	while done == False:
+		#main loop
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				done = True
+
+		screen.fill(BLACK)
+
+		ts.iterate()
+
+		for r, row in enumerate(ts.getState()):
+			model_square_map.append([])
+			for c, cell in enumerate(row):				
+				model_square_map[r][c].setColor(scaled_color(cell))
+
+		all_sprites_list.draw(screen)
+		pygame.display.flip()
+		clock.tick(120)
+	pygame.quit()
