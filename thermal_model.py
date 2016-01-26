807c2fcb20abda7d52fd8ca15d41f684e50b28b7
Rob Heath <rob@robheath.me.uk>
2016-01-26 16:27:15 +0000

    added thermal mass layer; added interactivity to UI

--- thermal_model.py
+++ thermal_model.py
@@ -4,28 +4,13 @@ import matplotlib.pyplot as plt
 
 class ThermalSystem:
 	def __init__(self):
-		self.model = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-					[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
+		self.model = np.zeros((40,40))
+		self.model[8][8] = 1 #sources of heat marked with 1
+		self.model[-3][-3] = -1 #heatsinks marked with -1
+
+		self.thermal_mass = np.ones_like(self.model,dtype=float) #coefficients for building material thermal properties
+		self.thermal_mass[5:-5,5:-5] += 100
+		self.thermal_mass[6:-6,6:-6] -= 100
 
 		self.rate = 10.0
 		self.base = 5.0
@@ -57,6 +42,18 @@ class ThermalSystem:
 				if r-1 >= 0:
 					self.neighbours[r][c].append([r-1,c])
 
+	def _toggleSource(self,r,c):
+		if [r,c] in self.sources:
+			self.sources.pop(self.sources.index([r,c]))
+		else:
+			self.sources.append([r,c])
+
+	def _toggleExhaust(self,r,c):
+		if [r,c] in self.exhausts:
+			self.exhausts.pop(self.exhausts.index([r,c]))
+		else:
+			self.exhausts.append([r,c])
+
 	def iterate(self):
 		for source in self.sources:
 			self.state[source[0],source[1]]+= self.rate
@@ -73,23 +70,36 @@ class ThermalSystem:
 						self.flows.append([neighbour,self.state[r][c]-self.state[neighbour[0]][neighbour[1]]])
 				if len(self.flows)>0:
 					for flow in self.flows:
-						self.flowstate[flow[0][0]][flow[0][1]]+=flow[1]/float(len(self.flows)+1)
-						self.flowstate[r][c]-=flow[1]/float(len(self.flows)+1)
+						self.change = flow[1]/(float(len(self.flows)+1))
+						self.flowstate[flow[0][0]][flow[0][1]]+=self.change/self.thermal_mass[flow[0][0]][flow[0][1]]
+						self.flowstate[r][c]-=self.change/self.thermal_mass[r][c]
 
 		self.state=np.add(self.state,self.flowstate)
 
 	def getState(self):
 		return self.state
 
+	def getThermalMass(self):
+		return self.thermal_mass
+
 	def shape(self):
 		return self.state.shape
 
+	def toggleSource(self,r,c):
+		if self.model[r][c] != 1:
+			self.model[r][c]=1
+		else:
+			self.model[r][c]=0
+		self._toggleSource(r,c)
 
-'''
-for i in range(100):
-	ts.iterate()
-	print ts.getState()
-'''
+	def toggleExhaust(self,r,c):
+		if self.model[r][c] != -1:
+			self.model[r][c]=-1
+		else:
+			self.model[r][c]=0
+		self._toggleExhaust(r,c)
+
+
 
 class Square(pygame.sprite.Sprite):
 	def __init__(self,x,y,w,h,color):
@@ -102,7 +112,31 @@ class Square(pygame.sprite.Sprite):
 	def setColor(self,color):
 		self.image.fill(color)
 
-def scaled_color(value):
+class GridSquare(Square):
+	def __init__(self,x,y,w,h,color,r,c):
+		Square.__init__(self,x,y,w,h,color)
+		self.r = r
+		self.c = c
+	def getRC(self):
+		return self.r, self.c
+
+class Button(Square):
+	def __init__(self,x,y,w,h,color,text):
+		Square.__init__(self,x,y,w,h,color)
+		self.updateText(text)
+	def updateText(self,text):
+		self.text = text
+		offset = font.size('Tg')[1]-5
+		for r, row in enumerate(self.text.split('\n')):
+			self.textSurf = font.render(row,20,BLACK)
+			self.image.blit(self.textSurf,(3,0+offset*r))
+	def setColor(self,color):
+		self.image.fill(color)
+		self.updateText(self.text)
+
+
+
+def scaled_color(value,maxima,minima):
 	'''
 	scale_factor = (255.0/float(maxima-minima))
 	return_value = []
@@ -132,36 +166,83 @@ if __name__=="__main__":
 	ts = ThermalSystem()
 	x_number, y_number = ts.shape()
 
-	WINDOW_AREA = (800,800)
+	WINDOW_AREA = (900,800)
+	CONTROLS = 100
 	BLACK = (0,0,0)
 	WHITE = (255,255,255)
+	RED = (255,100,100)
 	screen = pygame.display.set_mode(WINDOW_AREA)
 	pygame.display.set_caption('Thermal Model')
+	fontname = 'Garamond'
+	font = pygame.font.SysFont(fontname,20,True,False)
 	clock = pygame.time.Clock()
 
 	all_sprites_list = pygame.sprite.LayeredUpdates()
+	buttons_list = pygame.sprite.LayeredUpdates()
+
 	model_square_map = []
 	for r, row in enumerate(ts.getState()):
 		model_square_map.append([])
 		for c, cell in enumerate(row):
-			sq = Square(r*(WINDOW_AREA[0]/x_number)+border_size,c*(WINDOW_AREA[1]/y_number)+border_size,(WINDOW_AREA[0]/x_number)-2*border_size,(WINDOW_AREA[1]/y_number)-2*border_size,scaled_color(cell))
+			sq = GridSquare(r*((WINDOW_AREA[0]-CONTROLS)/x_number)+border_size,c*((WINDOW_AREA[1])/y_number)+border_size,(WINDOW_AREA[0]/x_number)-2*border_size,(WINDOW_AREA[1]/y_number)-2*border_size,scaled_color(cell,maxima,minima),r,c)
 			model_square_map[-1].append(sq)
 			all_sprites_list.add(sq)
+
+	material_view_button = Button((WINDOW_AREA[0]-CONTROLS)+4,1,CONTROLS-5,80-2,WHITE,'Material\nview')
+	buttons_list.add(material_view_button)
+	all_sprites_list.add(material_view_button)
+
+	LmbPressed = False
+	RmbPressed = False
+	ModelViewOn = False
+
 	done = False
 	while done == False:
 		#main loop
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				done = True
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				#print event.button
+				if event.button == 1:
+					LmbPressed = True
+				elif event.button == 3:
+					RmbPressed = True
+
+		if LmbPressed or RmbPressed:
+			under_mouse = [s for s in all_sprites_list.sprites() if s.rect.collidepoint(pygame.mouse.get_pos())]
+			if under_mouse != []:
+				for item in under_mouse:
+					if isinstance(item,GridSquare):
+						if LmbPressed:
+							ts.toggleSource(*item.getRC())
+							print ts.sources
+						elif RmbPressed:
+							ts.toggleExhaust(*item.getRC())
+							print ts.exhausts
+					if item is material_view_button:
+						ModelViewOn = not ModelViewOn
+						if ModelViewOn == True:
+							material_view_button.setColor(RED)
+						else:
+							material_view_button.setColor(WHITE)
+
+		LmbPressed = False
+		RmbPressed = False
 
 		screen.fill(BLACK)
 
-		ts.iterate()
-
-		for r, row in enumerate(ts.getState()):
-			model_square_map.append([])
-			for c, cell in enumerate(row):				
-				model_square_map[r][c].setColor(scaled_color(cell))
+		if ModelViewOn:
+			mvmaxima = ts.getThermalMass().max()
+			mvminima = ts.getThermalMass().min()
+			for r, row in enumerate(ts.getThermalMass()):
+				for c, cell in enumerate(row):
+					model_square_map[r][c].setColor(scaled_color(cell,mvmaxima,mvminima))
+		else:
+			ts.iterate()
+			for r, row in enumerate(ts.getState()):
+				for c, cell in enumerate(row):				
+					model_square_map[r][c].setColor(scaled_color(cell,maxima,minima))
 
 		all_sprites_list.draw(screen)
 		pygame.display.flip()
